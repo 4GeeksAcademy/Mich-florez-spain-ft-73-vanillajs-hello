183318b7ffad17fe8acce3b00699c5709b3e40d9
Michflorez <167869106+Michflorez@users.noreply.github.com>
2024-06-19 18:07:48 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,43 @@
 /* eslint-disable */
 import "bootstrap";
+
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {});
+  document.querySelector("#excuse").innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let excusegen = function(list) {
+    return Math.floor(Math.random() * list.length);
+  };
+  let whoIndex = excusegen(who);
+  let actionIndex = excusegen(action);
+  let whatIndex = excusegen(what);
+  let whenIndex = excusegen(when);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
